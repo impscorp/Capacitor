357f193bf53384b30c122d03e8185346e433f9d6
impscorp <jonvoegel@tsn.at>
2022-10-07 09:15:27 +0200

    Added winforms UI, And Methods to calculate the user inputs

--- CalcCapacitor.Lib/Capacitor.Lib.cs
+++ CalcCapacitor.Lib/Capacitor.Lib.cs
@@ -1,4 +1,6 @@
-﻿namespace CalcCapacitor.Lib;
+﻿using ValueService.Lib;
+
+namespace CalcCapacitor.Lib;
 
 public class Capacitor
 {
@@ -8,7 +10,10 @@ public class Capacitor
     #endregion
 
     #region Constructors
+    public Capacitor()
+    {
 
+    }
 
 
     #endregion
@@ -20,10 +25,7 @@ public class Capacitor
     /// <param name="v">volt</param>
     /// <param name="r">ohm</param>
     /// <returns></returns>
-    public static decimal ChargingCurrent(decimal v, decimal r)
-    {
-        return v / r;
-    }
+    public static decimal ChargingCurrent(decimal v, decimal r) => v / r;
 
     /// <summary>
     /// Calculates the load time of a capacitor
@@ -31,10 +33,7 @@ public class Capacitor
     /// <param name="r">ohm</param>
     /// <param name="c">farad</param>
     /// <returns></returns>
-    public static decimal LoadTime(decimal r, decimal c)
-    {
-        return r * c * 5;
-    }
+    public static decimal LoadTime(decimal r, decimal c) => r * c * 5;
     
     /// <summary>
     /// Calculates the Capacity of a capacitor
@@ -42,10 +41,26 @@ public class Capacitor
     /// <param name="tau">LoadTime / 5</param>
     /// <param name="r">ohm</param>
     /// <returns></returns>
-    public static decimal Capacity(decimal tau, decimal r)
+    public static decimal Capacity(decimal tau, decimal r) => tau / r;
+
+    public static decimal Resistance(decimal v, decimal i) => v / i;
+
+    public static decimal Voltage(decimal i, decimal r) => i * r;
+
+    public static decimal TimeConstant(decimal r, decimal c) => r * c;
+
+    public decimal Pow10PostFactor(decimal number, string PostFactor)
     {
-        return tau / r;
+        double.TryParse(Convert.ToString(GetPotenz(PostFactor)), out double dblpotenz);
+        return number * (decimal)Math.Pow(10.00d, dblpotenz);
     }
-    
+
+    public int? GetPotenz(string value)
+    {
+        ValueServices _vs = new ValueServices();
+        if (value == "") return 0;
+        return _vs.PostFactors.FirstOrDefault(element => element.TextShort == value)?.Potenz;
+    }
+
     #endregion
 }
--- CapacitatorUI/Form1.cs
+++ CapacitatorUI/Form1.cs
@@ -5,31 +5,44 @@ namespace CapacitatorUI
     public partial class Form1 : Form
     {
         ValueServices _vs = new ValueServices();
+        CalcCapacitor.Lib.Capacitor _cap = new CalcCapacitor.Lib.Capacitor();
         public Form1()
         {
-            InitializeComponent();
-            rPostFactor.DataSource = _vs.PostFactors.Select(x => x.Text += " Ω").ToList();
-            cPostFactor.DataSource = _vs.PostFactors.Select(x => x.Text += " F").ToList();
-            vPostFactor.DataSource = _vs.PostFactors.Select(x => x.Text += " V").ToList();
-            iPostFactor.DataSource = _vs.PostFactors.Select(x => x.Text += " A").ToList();
-            tPostFactor.DataSource = _vs.PostFactors.Select(x => x.Text += " s").ToList();
-            tauPostFactor.DataSource = _vs.PostFactors.Select(x => x.Text += " s").ToList();       
+            InitializeComponent();            
+            rPostFactor.DataSource = _vs.PostFactors.Select(x => x.TextShort += " Ω").ToList();
+            cPostFactor.DataSource = _vs.PostFactors.Select(x => x.TextShort.Replace(" Ω", " F")).ToList();
+            vPostFactor.DataSource = _vs.PostFactors.Select(x => x.TextShort.Replace(" Ω", " V")).ToList();
+            iPostFactor.DataSource = _vs.PostFactors.Select(x => x.TextShort.Replace(" Ω", " A")).ToList();
+            tPostFactor.DataSource = _vs.PostFactors.Select(x => x.TextShort.Replace(" Ω", " s")).ToList();
+            tauPostFactor.DataSource = _vs.PostFactors.Select(x => x.TextShort.Replace(" Ω", " s")).ToList();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            decimal r = Convert.ToDecimal(R.Text); string rpostfactor = rPostFactor.Text;
-            decimal v = Convert.ToDecimal(V.Text); string vpostfactor = vPostFactor.Text;
-            decimal c = Convert.ToDecimal(C.Text); string cpostfactor = cPostFactor.Text;
-            decimal i = Convert.ToDecimal(I.Text); string ipostfactor = iPostFactor.Text;
-            decimal t = Convert.ToDecimal(Tau.Text); string tpostfactor = tPostFactor.Text;
-            decimal tau = Convert.ToDecimal(Time.Text); string taupostfactor = tauPostFactor.Text;
-            
-            
-            r = _vs.Pow10PostFactor(r, rpostfactor);
-            c = _vs.Pow10PostFactor(c, cpostfactor);
-            v = _vs.Pow10PostFactor(v, vpostfactor);
+            //converts the user input to decimal
+            decimal r = string.IsNullOrWhiteSpace(R.Text) ? 0 : Convert.ToDecimal(R.Text); string rpostfactor = rPostFactor.SelectedItem.ToString().Replace("Ω", "").Trim();
+            decimal c = string.IsNullOrWhiteSpace(C.Text) ? 0 : Convert.ToDecimal(C.Text); string vpostfactor = vPostFactor.SelectedItem.ToString().Replace(" V", "").Trim();
+            decimal v = string.IsNullOrWhiteSpace(V.Text) ? 0 : Convert.ToDecimal(V.Text); string cpostfactor = cPostFactor.SelectedItem.ToString().Replace(" F", "").Trim();
+            decimal i = string.IsNullOrWhiteSpace(I.Text) ? 0 : Convert.ToDecimal(I.Text); string ipostfactor = iPostFactor.SelectedItem.ToString().Replace(" A", "").Trim();
+            decimal t = string.IsNullOrWhiteSpace(Time.Text) ? 0 : Convert.ToDecimal(Time.Text); string tpostfactor = tPostFactor.SelectedItem.ToString().Replace(" s", "").Trim();
+            decimal tau = string.IsNullOrWhiteSpace(Tau.Text) ? 0 : Convert.ToDecimal(Tau.Text); string taupostfactor = tauPostFactor.SelectedItem.ToString().Replace(" s", "").Trim();
 
+            //scales the user input to the base value
+            r = _cap.Pow10PostFactor(r, rpostfactor);
+            c = _cap.Pow10PostFactor(c, cpostfactor);
+            v = _cap.Pow10PostFactor(v, vpostfactor);
+            i = _cap.Pow10PostFactor(i, ipostfactor);
+            t = _cap.Pow10PostFactor(t, tpostfactor);
+            tau = _cap.Pow10PostFactor(tau, taupostfactor);
+            
+            //calculates the non empty values
+            if (r == 0) R.Text = Convert.ToString(CalcCapacitor.Lib.Capacitor.Resistance(v, i));
+            if (c == 0) C.Text = Convert.ToString(CalcCapacitor.Lib.Capacitor.Capacity(tau, r));
+            if (v == 0) V.Text = Convert.ToString(CalcCapacitor.Lib.Capacitor.Voltage(r, i));
+            if (i == 0) I.Text = Convert.ToString(CalcCapacitor.Lib.Capacitor.ChargingCurrent(v, r));
+            if (t == 0) Time.Text = Convert.ToString(CalcCapacitor.Lib.Capacitor.LoadTime(r, c));
+            if (tau == 0) Tau.Text = Convert.ToString(CalcCapacitor.Lib.Capacitor.TimeConstant(r, c));
         }
     }
 }
